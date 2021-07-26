610878af9a53e3e83ee66fb8e206f4acc60ecb64
Yan Soares <yts.1995@gmail.com>
2021-07-26 20:43:12 -0300

    chore: add correct interfaces

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,7 +2,7 @@ import { HttpGetClient } from '@/infra/http'
 
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get (args: HttpGetClient.Params): Promise<any> {
     const result = await axios.get(args.url, { params: args.params })
     return result.data
