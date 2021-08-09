09030f00b408b4a7f3a24f063c953dffe5b17dbb
Yan Soares <yts.1995@gmail.com>
2021-08-08 22:07:01 -0300

    chore: improve code legibility

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,9 +7,7 @@ export const setupRoutes = (app: Express): void => {
 
   readdirSync(join(__dirname, '../routes'))
     .filter(file => !file.endsWith('.map'))
-    .map(async file => {
-      (await import(`../routes/${file}`)).default(router)
-    })
+    .map(async file => (await import(`../routes/${file}`)).default(router))
 
   app.use('/api', router)
 }
