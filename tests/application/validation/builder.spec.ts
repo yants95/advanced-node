35dd997c28dfefbcaad7f982d451cf9838dda3bc
Yan Soares <yts.1995@gmail.com>
2021-09-04 15:13:53 -0300

    refactor: add Required validator

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,14 +1,14 @@
-import { RequiredStringValidator, ValidationBuilder } from '@/application/validation'
+import { RequiredString, ValidationBuilder } from '@/application/validation'
 
 describe('ValidationBuilder', () => {
-  it('should return a RequiredStringValidator', () => {
+  it('should return a RequiredString', () => {
     const validators = ValidationBuilder
       .of({ value: 'any_value', fieldName: 'any_name' })
       .required()
       .build()
 
     expect(validators).toEqual([
-      new RequiredStringValidator('any_value', 'any_name')
+      new RequiredString('any_value', 'any_name')
     ])
   })
 })
