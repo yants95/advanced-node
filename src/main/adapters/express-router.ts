5b70cb5d91b3ac29b089f7dfb7490ac8a555bf22
Yan Soares <yts.1995@gmail.com>
2021-09-11 14:48:42 -0300

    fix: fixig req.locals error

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,11 +4,8 @@ import { RequestHandler } from 'express'
 
 type Adapter = (controller: Controller) => RequestHandler
 
-export const adaptExpressRoute: Adapter = controller => {
-  return async (req, res) => {
-    const { statusCode, data } = await controller.handle({ ...req.body, ...req.locals })
-    const json = [200, 204].includes(statusCode) ? data : { error: data.message }
-
-    res.status(statusCode).json(json)
-  }
+export const adaptExpressRoute: Adapter = controller => async (req, res) => {
+  const { statusCode, data } = await controller.handle({ ...req.body, ...req.locals })
+  const json = [200, 204].includes(statusCode) ? data : { error: data.message }
+  res.status(statusCode).json(json)
 }
--- tests/infra/repos/postgres/mocks/connection.ts
+++ tests/infra/repos/postgres/mocks/connection.ts
@@ -4,9 +4,8 @@ export const makeFakeDB = async (entities?: any[]): Promise<IMemoryDb> => {
   const db = newDb()
   const connection = await db.adapters.createTypeormConnection({
     type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+    entities: entities ?? ['src/infra/repos/postgres/entities/index.ts']
   })
   await connection.synchronize()
-
   return db
 }
